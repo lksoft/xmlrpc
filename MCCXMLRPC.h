05076e151c9f056fdf404f7cfedbd9873f09d062
Scott Little <scott@littleknownsoftware.com>
2015-08-06 21:59:20 +0200

    Add MCC and make the framework build with a SIS namespace in front of it.

---
+++ MCCXMLRPC.h
@@ -0,0 +1,38 @@
+//
+//  MCCXMLRPC.h
+//  XMLRPC
+//
+//  Created by Little Known on 06/08/15.
+//
+//
+
+
+#import "MCCCommonHeader.h"
+
+//	Delegates & Protocols
+
+#define XMLRPCConnectionDelegate		MCC_PREFIXED_NAME(XMLRPCConnectionDelegate)
+#define XMLRPCEncoder					MCC_PREFIXED_NAME(XMLRPCEncoder)
+
+//	Objects
+
+#define XMLRPCConnection				MCC_PREFIXED_NAME(XMLRPCConnection)
+#define XMLRPCConnectionManager			MCC_PREFIXED_NAME(XMLRPCConnectionManager)
+#define XMLRPCDefaultEncoder			MCC_PREFIXED_NAME(XMLRPCDefaultEncoder)
+#define XMLRPCEventBasedParser			MCC_PREFIXED_NAME(XMLRPCEventBasedParser)
+#define XMLRPCEventBasedParserDelegate	MCC_PREFIXED_NAME(XMLRPCEventBasedParserDelegate)
+#define XMLRPCRequest					MCC_PREFIXED_NAME(XMLRPCRequest)
+#define XMLRPCResponse					MCC_PREFIXED_NAME(XMLRPCResponse)
+
+//	Constants & Types
+#define XMLRPCElementType				MCC_PREFIXED_NAME(XMLRPCElementType)
+#define XMLRPCElementTypeArray			MCC_PREFIXED_NAME(XMLRPCElementTypeArray)
+#define XMLRPCElementTypeDictionary		MCC_PREFIXED_NAME(XMLRPCElementTypeDictionary)
+#define XMLRPCElementTypeMember			MCC_PREFIXED_NAME(XMLRPCElementTypeMember)
+#define XMLRPCElementTypeName			MCC_PREFIXED_NAME(XMLRPCElementTypeName)
+#define XMLRPCElementTypeInteger		MCC_PREFIXED_NAME(XMLRPCElementTypeInteger)
+#define XMLRPCElementTypeDouble			MCC_PREFIXED_NAME(XMLRPCElementTypeDouble)
+#define XMLRPCElementTypeBoolean		MCC_PREFIXED_NAME(XMLRPCElementTypeBoolean)
+#define XMLRPCElementTypeString			MCC_PREFIXED_NAME(XMLRPCElementTypeString)
+#define XMLRPCElementTypeDate			MCC_PREFIXED_NAME(XMLRPCElementTypeDate)
+#define XMLRPCElementTypeData			MCC_PREFIXED_NAME(XMLRPCElementTypeData)
